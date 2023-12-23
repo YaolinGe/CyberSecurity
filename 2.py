2c3d05b0677537271f80c5236cccdf5a68a361e5
Yaolin Ge <geyaolin@gmail.com>
2023-12-23 10:54:59 +0100

    update chapter 3

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 """
-This script is trying to decrypt the information of a text. 
+This script is trying to decrypt the information of a text.
 
 Author: Yaolin Ge
 Email: geyaolin@gmail.com
@@ -7,28 +7,28 @@ Date: 2023-10-10
 """
 
 
-def decipher_text(text, n): 
+def decipher_text(text, n):
     """
     This function is trying to decipher the text.
     """
     text_deciphered = ''
     texts = text.split(' ')
-    for text in texts: 
+    for text in texts:
         print(text)
-        for l in text: 
+        for l in text:
             # only shift it in the alphabet, not in ascii
             if ord(l) >= 97 and ord(l) <= 122: # lower case
                 text_deciphered += chr((ord(l) - 97 + n) % 26 + 97)
             elif ord(l) >= 65 and ord(l) <= 90: # upper case
                 text_deciphered += chr((ord(l) - 65 + n) % 26 + 65)
-            else: 
+            else:
                 text_deciphered += l
         text_deciphered += ' '
     # reoganize the text
     print(text_deciphered)
-            
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     # print("Hello World!")
-    # for i in range(26): 
-    decipher_text("Otkz D zvmizy v amzz kjdio! di ocz wjs wzgjr.", 8)
+    for i in range(26):
+        decipher_text("Otkz D zvmizy v amzz kjdio! di ocz wjs wzgjr.", i)
---
+++ notes.md
@@ -0,0 +1,30 @@
+# Notes for cybersecurity
+
+## Securing accounts
+
+## Securing data
+
+## Securing systems
+- WPA (wifi protected access (WPA))
+- HTTP (machine-in-the-middle attacks) if only http is used.
+- HTML
+- Packet sniffing
+- Cookies
+- Session hijacking
+- HTTPS (SSL, TLS, X.509, CA(signature hashing))
+- SSL stripping
+- HSTS (HTTP Strict Transport Security)
+- VPN
+- SSH
+- Port, 80(HTTP), 443(HTTPS), 22(SSH)
+- Port scanning
+- Penetration testing (Pen-testing)
+- Ethical hacking (red team: attack, blue team: defend)
+- Firewall (IP adress, deep packet inspection, proxy, url rewritting)
+- Malware (virus, worm, botnet, denial-of-service attack (DOS attack), distributed denial-of-service attack (DDoS))
+- Antivirus (automatic updates)
+- Zero-day attack
+
+## Securing software
+- Phishing
+- Code injection (cross-site scripting (xss))
